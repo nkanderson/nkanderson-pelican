8578fc41a5029a88de24069b5707563538528d92
Niklas Anderson <nkanders@gmail.com>
2022-01-26 21:39:26 -0800

    New pelican site with basic theme and content

---
+++ pelicanconf.py
@@ -0,0 +1,34 @@
+AUTHOR = 'Niklas Anderson'
+SITENAME = 'Niklas Anderson | Software Engineer'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'America/Los_Angeles'
+
+DEFAULT_LANG = 'en'
+
+THEME = 'nik-custom'
+DISPLAY_CATEGORIES_ON_MENU = False
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'https://getpelican.com/'),
+         ('Python.org', 'https://www.python.org/'),
+         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 15
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
